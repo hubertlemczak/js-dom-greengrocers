4092bbb011bd1c20d073dd4d20583acf1dcf00e6
Hubert Lemczak <hubertlemczak@gmail.com>
2022-06-29 16:21:24 +0100

    core complete

--- index.js
+++ index.js
@@ -1,55 +1,197 @@
 const state = {
   items: [
     {
-      id: "001-beetroot",
-      name: "beetroot",
-      price: 0.35
+      id: '001-beetroot',
+      name: 'beetroot',
+      price: 0.35,
     },
     {
-      id: "002-carrot",
-      name: "carrot",
-      price: 0.35
+      id: '002-carrot',
+      name: 'carrot',
+      price: 0.35,
     },
     {
-      id: "003-apple",
-      name: "apple",
-      price: 0.35
+      id: '003-apple',
+      name: 'apple',
+      price: 0.35,
     },
     {
-      id: "004-apricot",
-      name: "apricot",
-      price: 0.35
+      id: '004-apricot',
+      name: 'apricot',
+      price: 0.35,
     },
     {
-      id: "005-avocado",
-      name: "avocado",
-      price: 0.35
+      id: '005-avocado',
+      name: 'avocado',
+      price: 0.35,
     },
     {
-      id: "006-bananas",
-      name: "bananas",
-      price: 0.35
+      id: '006-bananas',
+      name: 'bananas',
+      price: 0.35,
     },
     {
-      id: "007-bell-pepper",
-      name: "bell pepper",
-      price: 0.35
+      id: '007-bell-pepper',
+      name: 'bell pepper',
+      price: 0.35,
     },
     {
-      id: "008-berry",
-      name: "berry",
-      price: 0.35
+      id: '008-berry',
+      name: 'berry',
+      price: 0.35,
     },
     {
-      id: "009-blueberry",
-      name: "blueberry",
-      price: 0.35
+      id: '009-blueberry',
+      name: 'blueberry',
+      price: 0.35,
     },
     {
-      id: "010-eggplant",
-      name: "eggplant",
-      price: 0.35
-    }
+      id: '010-eggplant',
+      name: 'eggplant',
+      price: 0.35,
+    },
   ],
-  cart: []
+  cart: [],
+  total: `£${(0).toFixed(2)}`,
+  sortByType: false,
+  sortAlphabetically: false,
 };
+
+const setState = (newState) => {
+  Object.keys(newState).forEach((key) => {
+    state[key] = newState[key];
+  });
+  render();
+};
+
+const addItemToCart = (item) => {
+  if (!isInCart(item.id)) {
+    item.quantity = 1;
+    const newCart = [...state.cart];
+    newCart.push(item);
+    setState({ cart: newCart });
+  } else {
+    item.quantity++;
+    render();
+  }
+  console.log(state.cart);
+};
+
+const removeItemFromCart = (itemId) => {
+  const newCart = [...state.cart].filter((item) => item.id !== itemId);
+  setState({ cart: newCart });
+};
+
+const isInCart = (itemId) => {
+  let result = false;
+  state.cart.forEach((item) => {
+    if (item.id === itemId) result = true;
+  });
+  return result;
+};
+
+const createStoreItem = (item) => {
+  const li = document.createElement('li');
+  const div = document.createElement('div');
+  const img = document.createElement('img');
+  const button = document.createElement('button');
+
+  div.classList.add('store--item-icon');
+  img.setAttribute('src', `./assets/icons/${item.id}.svg`);
+  img.setAttribute('alt', `${item.name}`);
+  button.addEventListener('click', () => {
+    console.log(`adding ${item.name} to cart`);
+    addItemToCart(item);
+  });
+  button.innerText = 'Add to cart';
+
+  const storeProducts = document.querySelector('.store--item-list');
+  storeProducts.append(li);
+  li.append(div);
+  div.append(img);
+  li.append(button);
+};
+
+const renderStoreItems = () => {
+  const storeProducts = document.querySelector('.store--item-list');
+  storeProducts.innerHTML = '';
+  state.items.forEach((item) => {
+    createStoreItem(item);
+  });
+};
+
+const createCartItem = (item) => {
+  const li = document.createElement('li');
+  const img = document.createElement('img');
+  const p = document.createElement('p');
+  const decrement = document.createElement('button');
+  const span = document.createElement('span');
+  const increment = document.createElement('button');
+
+  img.classList.add('cart--item-icon');
+  img.setAttribute('src', `./assets/icons/${item.id}.svg`);
+  img.setAttribute('alt', `${item.name}`);
+
+  p.innerText = `${item.name}`;
+
+  decrement.addEventListener('click', () => {
+    item.quantity--;
+    if (item.quantity <= 0) removeItemFromCart(item.id);
+    render();
+  });
+  decrement.innerText = '-';
+  decrement.classList.add('.quantity-btn', 'remove-btn', 'center');
+
+  span.classList.add('.quantity-text', 'center');
+  span.innerText = item.quantity;
+
+  increment.addEventListener('click', () => {
+    item.quantity++;
+    render();
+  });
+  increment.innerText = '+';
+  increment.classList.add('.quantity-btn', 'add-btn', 'center');
+
+  const cartProducts = document.querySelector('.cart--item-list');
+  cartProducts.append(li);
+  li.append(img);
+  li.append(p);
+  li.append(decrement);
+  li.append(span);
+  li.append(increment);
+};
+
+const calculateCartTotal = () => {
+  let sum = 0;
+  state.cart.forEach((item) => {
+    sum += item.price * item.quantity;
+  });
+  return sum;
+};
+
+const renderCartItems = () => {
+  const cartProducts = document.querySelector('.cart--item-list');
+  cartProducts.innerHTML = '';
+  state.cart.forEach((item) => {
+    createCartItem(item);
+  });
+};
+
+const renderTotal = () => {
+  const cartTotal = calculateCartTotal();
+  const total = document.querySelector('.total-number');
+  state.total = `£${cartTotal.toFixed(2)}`;
+  total.innerText = state.total;
+};
+
+const render = () => {
+  renderStoreItems();
+  renderCartItems();
+  renderTotal();
+};
+
+const init = () => {
+  render();
+};
+
+init();
